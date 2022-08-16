03daa456fe4655d75456329e2b3684df3d5c6dcb
hoangks5 <hoangnd.ute@gmail.com>
2022-08-16 14:16:43 +0700

    hoangks5-update

--- show_price_v2.py
+++ show_price_v2.py
@@ -48,37 +48,6 @@ def coinbase_benmark(token):
 
 
 
-def main1(token):
-    datas = mycol.find({})
-    median = []
-    vwa = []
-    coinbase = []
-    chainlink = []
-    timestamp = []
-    price_min = []
-    price_max = []
-    price_noise = []
-    for data in datas:
-        if data['data']['token'] == token:
-            median.append(data['data']['price_median'])
-            vwa.append(data['data']['price_volume_weighted_average'])
-            coinbase.append(data['data']['price_coinbase'])
-            chainlink.append(data['data']['price_chainlink'])
-            timestamp.append(data['data']['timestamp'])
-            price_min.append(data['data']['price_min'])
-            price_max.append(data['data']['price_max'])
-            price_noise.append(data['data']['price_gaussian_noise'])
-    median = np.array(median)
-    vwa = np.array(vwa)
-    chainlink = np.array(chainlink)
-    timestamp = np.array(timestamp)
-    coinbase = np.array(coinbase)
-    plt.plot(timestamp,median,timestamp,vwa,timestamp,price_min,
-            timestamp,price_max,timestamp,price_noise)
-    plt.title(token.split('-')[0])
-    plt.ylabel('USD')
-    frame = plt.gca()
-    frame.axes.get_xaxis().set_visible(False)
 for token in TOKENS:
     coinbase_benmark(token)
 plt.gca().legend(('Median','Volume Weighted Average','Min','Max','Noise'))
