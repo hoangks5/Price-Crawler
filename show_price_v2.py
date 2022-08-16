e5403be30af95c6bc2623a86c2b7912547991993
hoangks5 <hoangnd.ute@gmail.com>
2022-08-16 14:09:13 +0700

    hoangks5-update

--- show_price_v2.py
+++ show_price_v2.py
@@ -16,7 +16,6 @@ def coinbase_benmark(token):
     median = [] 
     vwa = []
     coinbase = []
-    chainlink = []
     timestamp = []
     price_min = []
     price_max = []
@@ -26,7 +25,6 @@ def coinbase_benmark(token):
             median.append(data['data']['price_median'])
             vwa.append(data['data']['price_volume_weighted_average'])
             coinbase.append(data['data']['price_coinbase'])
-            chainlink.append(data['data']['price_chainlink'])
             timestamp.append(data['data']['timestamp'])
             price_min.append(data['data']['price_min'])
             price_max.append(data['data']['price_max'])
