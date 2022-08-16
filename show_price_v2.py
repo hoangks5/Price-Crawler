f4385778ce25fcf547d8de3a3bfca007e104c6eb
hoangks5 <hoangnd.ute@gmail.com>
2022-08-16 14:11:43 +0700

    hoangks5-update

--- show_price_v2.py
+++ show_price_v2.py
@@ -34,6 +34,8 @@ def coinbase_benmark(token):
     coinbase = np.array(coinbase) - np.array(coinbase)
     median = np.array(median) - np.array(coinbase)
     vwa = np.array(vwa) - np.array(coinbase)
+
+    
     plt.subplot(2,4,TOKENS.index(token)+1)
     plt.plot(timestamp,median,timestamp,vwa,timestamp,coinbase,timestamp,timestamp,price_max,timestamp,price_min,timestamp,price_noise)
     plt.title(token.split('-')[0])
