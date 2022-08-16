c3747a505f4addf44c27799cfe5dba80c88f084f
hoangks5 <hoangnd.ute@gmail.com>
2022-08-16 09:15:43 +0700

    hoangks5-update

--- show_price.py
+++ show_price.py
@@ -77,7 +77,7 @@ def main1(token):
     frame = plt.gca()
     frame.axes.get_xaxis().set_visible(False)
 
-
+ 
 for token in TOKENS:
     main(token)
 plt.gca().legend(('Median','Volume Weighted Average','Coinbase','Chainlink','Min','Max','Noise'))
